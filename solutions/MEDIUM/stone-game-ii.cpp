723770ae7b6fc59ca30357fe6c3699179e656c0c
aunabbas2006 <aunabbas0807@gmail.com>
2026-08-09 17:01:47 +0500

    Add solution: Stone Game II (MEDIUM)

--- solutions/MEDIUM/stone-game-ii.cpp
+++ solutions/MEDIUM/stone-game-ii.cpp
@@ -1,46 +1,45 @@
 // Problem: Stone Game II
 // Difficulty: MEDIUM
 // Link: https://leetcode.com/problems/stone-game-ii/
-// Approach: Dynamic programming with memoization is used to calculate the maximum stones the current player can obtain from a given starting pile `i` and maximum `X` value `M`. The recurrence involves iterating through all valid `X` choices, adding the stones taken, and subtracting the optimal score the opponent would achieve from the remaining piles.
+// Approach: Dynamic programming with memoization, where `dp[i][m]` stores the maximum stones the current player can obtain from `piles[i:]` with the current `M` value, assuming optimal play by both Alice and Bob.
 
 class Solution {
 public:
     int N;
-    vector<int> suffix_sum;
-    vector<vector<int>> dp;
+    vector<int> Piles;
+    vector<int> SuffixSum;
+    vector<vector<int>> Memo;
 
-    int solve(int i, int m) {
-        if (i == N) {
+    int solve(int idx, int m) {
+        if (idx == N) {
             return 0;
         }
-        if (dp[i][m] != -1) {
-            return dp[i][m];
+        if (Memo[idx][m] != -1) {
+            return Memo[idx][m];
         }
 
-        int res = 0;
-        
-        int current_total_stones_from_i_onwards = suffix_sum[i];
-
-        for (int X = 1; X <= min(2 * m, N - i); ++X) {
-            int next_i = i + X;
-            int next_m = max(m, X);
-            
-            int score_current_player = current_total_stones_from_i_onwards - solve(next_i, next_m);
-            res = max(res, score_current_player);
+        int maxStones = 0; 
+        for (int X = 1; X <= 2 * m; ++X) {
+            if (idx + X > N) {
+                break;
+            }
+            int currentStones = SuffixSum[idx] - solve(idx + X, max(m, X));
+            maxStones = max(maxStones, currentStones);
         }
 
-        return dp[i][m] = res;
+        return Memo[idx][m] = maxStones;
     }
 
     int stoneGameII(vector<int>& piles) {
         N = piles.size();
+        Piles = piles;
 
-        suffix_sum.resize(N + 1, 0);
+        SuffixSum.resize(N + 1, 0);
         for (int i = N - 1; i >= 0; --i) {
-            suffix_sum[i] = piles[i] + suffix_sum[i + 1];
+            SuffixSum[i] = Piles[i] + SuffixSum[i + 1];
         }
 
-        dp.resize(N + 1, vector<int>(N + 1, -1));
+        Memo.resize(N + 1, vector<int>(N + 1, -1));
 
         return solve(0, 1);
     }
